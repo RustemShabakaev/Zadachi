353a7d7b040875f96fa1ba2938ee6f8c3647f3ce
rustem <prorusteg@gmail.com>
2022-11-18 19:59:53 +0300

    10

--- DomashneeZadanie1/Program.cs
+++ DomashneeZadanie1/Program.cs
@@ -7,7 +7,34 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.WriteLine("Введите Трехзначное число");
+Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+int result1 = number ;
+int result2 = number ;
+int count1 = 0;
+int count2 =0;
+
+if ((number / 100) == 0)
+{
+    Console.WriteLine($"{number} --> Третьей цифры нет ");
+}
+else
+{
+        while ((result1 / 10) > 0)
+    {
+        result1 = result1 / 10;
+        count1++;
+    }
+
+         while
+        (count2 < (count1-2))
+    { 
+    result2 = result2 / 10;
+    count2++;
+    }
+    int itog= result2 % 10;
+    Console.WriteLine ($"3е число числа {number} ----> {itog}  ");
+}
+
 
 
